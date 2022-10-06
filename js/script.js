45dca8f936ac70d8b5cf5abfeac8084f9af5b009
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-06 12:00:29 -0300

    Support stringing together of several operations

--- js/script.js
+++ js/script.js
@@ -18,49 +18,50 @@ function operate(operator, a, b) {
   return operator(a, b);
 }
 
-const display = document.querySelector('.display-text');
-const keys = document.querySelector('.keys');
-const btns = document.querySelectorAll('btn');
+const display = document.querySelector(".display");
+const keys = document.querySelector(".keys");
+const btns = document.querySelectorAll("btn");
 
-let displayValue = '';
-let a;
-let b;
+let a = "";
+let b = "";
+let c = "";
 let operator;
-keys.addEventListener('click', function (e) {
-  if (e.target.matches('.number')) {
-    if (display.textContent == 0) display.textContent = '';
-    display.textContent += e.target.textContent;
-    displayValue += e.target.textContent;
-    console.log(displayValue);
-  } else if (e.target.matches('.operator')) {
-    
-    a = +displayValue;
-    displayValue = '';
+let operand = "";
+
+keys.addEventListener("click", function (e) {
+  if (e.target.matches(".number")) {
+    operand += e.target.textContent;
+    display.textContent += operand.slice(-1);
+  } else if (e.target.matches(".operator")) {
+    if (e.target.matches(".equals") || typeof a === "number") {
+      b = +operand;
+      c = operate(operator, a, b);
+      a = c;
+      b = "";
+      operand = "";
+      display.textContent = "";
+      display.textContent += c;
+    } else {
+      a = +operand;
+      operand = "";
+    }
     switch (true) {
-      case e.target.matches('.add'):
+      case e.target.matches(".add"):
         operator = add;
-        display.textContent += ' + ';
+        display.textContent += " + ";
         break;
-      case e.target.matches('.subtract'):
+      case e.target.matches(".subtract"):
         operator = subtract;
-        display.textContent += ' - ';
+        display.textContent += " - ";
         break;
-      case e.target.matches('.multiply'):
+      case e.target.matches(".multiply"):
         operator = multiply;
-        display.textContent += ' * ';
+        display.textContent += " * ";
         break;
-      case e.target.matches('.divide'):
+      case e.target.matches(".divide"):
         operator = divide;
-        console.log(operator);
-        display.textContent += ' / ';
+        display.textContent += " / ";
         break;
     }
-  } else if (e.target.matches('.equals')) {
-    b = +displayValue;
-      console.log(a);
-      console.log(b);
-      console.log(operate);
-      display.textContent = operate(operator, a, b);
   }
 });
-
